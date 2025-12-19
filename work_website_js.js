20585d232804c3d1a0af2f9ac83d89ae232dbae9
treebee123 <77751062+treebee123@users.noreply.github.com>
2025-12-19 11:12:59 +0000

    import videos
    add videos and the main bulk of the website

--- work_website_js.js
+++ work_website_js.js
@@ -1,15 +1,49 @@
-setTimeout(100); //allows the css to load
+
+setTimeout(100);
 
 //makes the borders height equal the same height as the web page
 function setBordersHeight() {
-    document.body.height = "1500px";
+    //document.body.height = "1500px";
 
     const borders = document.getElementsByClassName("border");
 
     for (const border of borders) {
-        console.log("set " + border.id + " height to " + document.body.height);
-        border.style.height = document.body.height;
+        //console.log("set " + border.id + " height to " + height);
+        //border.style.height = document.body.height;
+        border.style.height = (document.documentElement.scrollHeight) + "px";
     }
+
+    //document.getElementById("bottomBar").style.bottom = document.body.bottom;
 }
 
-setBordersHeight();
+function maxHeight() {
+    return Math.max(body.scrollHeight, body.offsetHeight,
+        html.clientHeight, html.scrollHeight, html.offsetHeight);
+}
+
+//function rgbToHex(r, g, b) {
+//    return '#' + [r, g, b].map(x => x.toString(16).padStart(2, '0')).join('');
+//}
+
+//function changeBottomColour() {
+//    var gradient = new Rainbow();
+//    gradient.setNumberRange(0, document.documentElement.scrollHeight - window.innerHeight);
+//    gradient.setSpectrum(rgbToHex(77, 77, 77), rgbToHex(227, 227, 227));
+
+//    document.getElementById("bottomBar").style.backgroundColor = '#' + gradient.colorAt(window.scrollY / 5);
+//}
+
+//document.addEventListener("scroll", changeBottomColour);
+
+document.addEventListener('resize', () => {
+    document.body.style.height = (document.documentElement.scrollHeight + 75) + "px";
+    setBordersHeight();
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.body.style.height = (document.documentElement.scrollHeight + 75) + "px";
+    setBordersHeight();
+});
+
+//document.body.style.height = (document.documentElement.scrollHeight + 75) + "px";
+//setBordersHeight();
