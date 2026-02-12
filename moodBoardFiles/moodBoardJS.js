ff73402e105584330a14a8c50076c749cf43fb4c
treebee123 <77751062+treebee123@users.noreply.github.com>
2026-02-12 22:58:35 +0000

    full rework layout now works
    changed to creating a whole array of column parent elements where we then load an image to one by one

--- moodBoardFiles/moodBoardJS.js
+++ moodBoardFiles/moodBoardJS.js
@@ -12,15 +12,72 @@ window.onload = function() {
 }
 
 const changeColSlider = (event) => {
-    console.log("pictures in column: ", event.target.value);
+    //console.log("pictures in column: ", event.target.value);
     document.getElementById("amountInColLabel").innerText = "Pictures in column: " + event.target.value;
-    document.getElementById("imageContainer").style.gridTemplateColumns = `repeat(${event.target.value}, minmax(0, 1fr))`;
+    //document.getElementById("imageContainer").style.gridTemplateColumns = `repeat(${event.target.value}, minmax(0, 1fr))`;
+
+    const container = document.getElementById("imageContainer");
+
+    const images = [...document.getElementsByClassName("moodImage")];
+
+    container.replaceChildren();
+
+    for (i = 0; i < event.target.value; i++) {
+        const newColumn = createFragment(`<div id="column${i}" class="column"></div>`);
+        document.getElementById("imageContainer").appendChild(newColumn);
+    }
+
+    let columnCount = 0;
+    for (let image of images) {
+        const currentCol = columnCount % event.target.value;
+        const newImage = image;
+        document.getElementById(`column${currentCol}`).appendChild(newImage);
+
+        columnCount++;
+    }
+
+    columns = document.getElementsByClassName("column");
+    for (i = 0; i < event.target.value; i++) {
+        columns[i].style.msFlex = `${100 / event.target.value}%`;  // IE10
+        columns[i].style.flex = `${100 / event.target.value}%`;
+    }
 }
 
 const loadFiles = (event) => {
-    const files = event.target.files;
-    for(let file of files){
-        const newImage = createFragment(`<image src="${URL.createObjectURL(file)}">`);
-        document.getElementById("imageContainer").appendChild(newImage);
+    const columnsLength = document.getElementById("amountInColSlider").value;
+
+    for (i = 0; i < columnsLength; i++) {
+        const newColumn = createFragment(`<div id="column${i}" class="column"></div>`);
+        document.getElementById("imageContainer").appendChild(newColumn);
     }
+
+    const files = event.target.files;
+    let columnCount = 0;
+    for (let file of files) {
+        const currentCol = columnCount % columnsLength;
+        const newImage = createFragment(`<image src="${URL.createObjectURL(file)}" style="width:100%;" class="moodImage">`);
+        document.getElementById(`column${currentCol}`).appendChild(newImage);
+
+        columnCount++;
+    }
+
+    const columns = document.getElementsByClassName("column");
+    for (i = 0; i < columnsLength; i++) {
+        columns[i].style.msFlex = `${100 / columnsLength}%`;  // IE10
+        columns[i].style.flex = `${100 / columnsLength}%`;
+    }
+}
+
+function downloadGrid() {
+    const grid = document.getElementById("imageContainer");
+
+    html2canvas(grid, {
+        scale: 2, // increase for higher resolution (important for A4)
+        useCORS: true
+    }).then(canvas => {
+        const link = document.createElement("a");
+        link.download = "grid.png";
+        link.href = canvas.toDataURL("image/png");
+        link.click();
+    });
 }
