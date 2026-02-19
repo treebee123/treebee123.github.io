47f5a942b8ec6e344978c482b1b38a556bed945f
treebee123 <77751062+treebee123@users.noreply.github.com>
2026-02-19 20:30:16 +0000

    setup the variables for the edit and preview
    input text that appears on the preview side of the page

--- invoiceCreator/invoiceCreatorJS.js
+++ invoiceCreator/invoiceCreatorJS.js
@@ -1,9 +1,17 @@
-function updateInvoiceSize() {
+const invoiceContainer = document.getElementById("invoiceContainers");
 
-    const invoiceContainer = document.getElementById("invoiceContainer");
+function updateInvoiceContainerSize() {
+    const YPos = invoiceContainer.getBoundingClientRect().y;
 
-    const maxHeight = window.innerHeight - 100;
-    const maxWidth = window.innerWidth - 100;
+    const newHeight = document.documentElement.clientHeight - YPos;
+
+    invoiceContainer.style.height = (newHeight - 20) + "px";
+}
+
+function updateInvoicesSize() {
+    const maxHeight = invoiceContainer.getBoundingClientRect().height;
+    const maxWidth = invoiceContainer.getBoundingClientRect().width / 2;
+
 
     const width = 794;  
     const height = 1123;
@@ -17,8 +25,78 @@ function updateInvoiceSize() {
         newWidth = newHeight / ratio;
     }
 
-    invoiceContainer.style.width = newWidth + "px";
-    invoiceContainer.style.height = newHeight + "px";
+    const invoiceNames = ["invoiceEditContainer", "invoicePreviewContainer"];
+    for (const itemName of invoiceNames) {
+        const invoiceItem = document.getElementById(itemName);
+
+        invoiceItem.style.width = newWidth + "px";
+        invoiceItem.style.height = newHeight + "px";
+    }
 }
-window.addEventListener('resize', updateInvoiceSize);
-updateInvoiceSize();
+
+function updatePreviews() {
+    const allElements = document.getElementsByTagName("*");
+
+    for (const element of allElements) {
+        const elementID = element.id;
+
+        if (elementID.startsWith("preview")) {
+            const idName = element.id.replace("preview", "edit");
+            const opposingElement = document.getElementById(`${idName}`)
+
+            if (element.textContent != opposingElement.value) {
+                element.textContent = opposingElement.value;
+            }
+        }
+    }
+}
+
+function confineInputs() {
+    const allInputs = document.getElementsByTagName("input");
+    
+    for (const input of allInputs) {
+        const words = input.value;
+        let sinceLastSpace = 0;
+
+        let newWord = "";
+        for (const letter of words) {
+            newWord += letter;
+
+            if (letter == ' ') {
+                sinceLastSpace = 0;
+                continue;
+            }
+
+
+            const opposingElement = document.getElementById(input.id.replace("edit", "preview"))
+            let opposingFontSize = parseFloat(getComputedStyle(opposingElement).fontSize);
+
+            let viewWidth = (100 * opposingFontSize) / window.innerWidth;
+
+
+            if (sinceLastSpace >= (18 / viewWidth)) {
+                newWord = newWord.substring(0, newWord.length - 1);
+                break;
+            }
+
+            sinceLastSpace++;
+        }
+
+        input.value = newWord;
+    }
+}
+
+window.addEventListener('resize', () => {
+    updateInvoiceContainerSize();
+    updateInvoicesSize();
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    updateInvoiceContainerSize();
+    updateInvoicesSize();
+});
+
+setInterval(() => {
+    confineInputs();
+    updatePreviews();
+}, 10);
