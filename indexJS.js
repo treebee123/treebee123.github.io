c228817134b1f0aed3582921f6531546e5bd1463
treebee123 <77751062+treebee123@users.noreply.github.com>
2026-05-18 15:06:09 +0100

    update for proper paths and allow a poster path
    posters are fill ins for when the video hasn't loaded yet.
    
    Poster made for the notepad and the moodboard

--- indexJS.js
+++ indexJS.js
@@ -14,17 +14,17 @@ function makeStruct(variables) {
     return constructor;
 }
 
-var Item = makeStruct("videoPath htmlPath title description");
+var Item = makeStruct("videoPath posterPath htmlPath title description");
 var projectsToLoad = [
-    new Item("Assets/videos/moodboard/moodboardVideo.mp4", "moodBoardFiles/moodBoardHTML.html", "Mood Board Creator", "A quick and easy way to create and print a mood board. Mood board may clip images if outside of area."),
+    new Item("Assets/mainMenuAssets/moodboard/moodboardVideo.mp4", "Assets/mainMenuAssets/moodboard/moodboardPoster.png", "moodBoardFiles/moodBoardHTML.html", "Mood Board Creator", "A quick and easy way to create and print a mood board. Mood board may clip images if outside of area."),
     //new Item("", "invoiceCreator/invoiceCreatorHTML.html", "Invoice Creator", "Use a given template to create a multitude of invoices."),
-    new Item("", "notepadFiles/notepadHTML.html", "Notepad", "To store any notes that may arise")
+    new Item("", "Assets/mainMenuAssets/notepad/notepadPoster.png", "notepadFiles/notepadHTML.html", "Notepad", "To store any notes that may arise")
 ]
 
 for (let projectIndex = 0; projectIndex < projectsToLoad.length; projectIndex++) {
     const projectItem = projectsToLoad[projectIndex];
 
-    const newButton = createFragment(`<div class="button relative"> <video class="buttonVideo relative" autoplay muted loop> <source src="${projectItem.videoPath}"> </video> <div class="buttonTextHolder absolute centered"> <h2 class="buttonTitle">${projectItem.title}</h2> <p class="buttonText">${projectItem.description}</p> </div> </div>`);
+    const newButton = createFragment(`<div class="button relative"> <video class="buttonVideo relative" poster="${projectItem.posterPath}" autoplay muted loop> <source src="${projectItem.videoPath}"> </video> <div class="buttonTextHolder absolute centered"> <h2 class="buttonTitle">${projectItem.title}</h2> <p class="buttonText">${projectItem.description}</p> </div> </div>`);
     newButton.addEventListener("click", () => { window.location.href = `${projectItem.htmlPath}` });
 
     document.getElementById("HTMLButtonHolder").appendChild(newButton);
